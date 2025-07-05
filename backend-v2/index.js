18f425bfcb49d6a2821c44472be466dc9546e086
willzhang <willzhmic@outlook.com>
2025-07-06 00:05:53 +0800

    update

--- backend-v2/index.js
+++ backend-v2/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-app.use(cors());
-app.get('/', (req, res) => {
+app.get('/admin', (req, res) => {
   res.json({ version: 'v2', message: 'Hello from backend v2!' });
 });
-app.listen(3000, () => console.log('Backend v2 running on 3000')); 
+app.listen(3000, () => console.log('Backend v2 running on 3000'));
